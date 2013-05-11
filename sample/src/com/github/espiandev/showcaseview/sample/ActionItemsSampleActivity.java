721b4749adbbfad25c69dffb3e50abb683f6c740
Blundell <blundell.app.dev@gmail.com>
2013-05-11 10:09:22 +0100

    Removed sample dependancy on ActionBarSherlock

--- sample/src/com/github/espiandev/showcaseview/sample/ActionItemsSampleActivity.java
+++ sample/src/com/github/espiandev/showcaseview/sample/ActionItemsSampleActivity.java
@@ -1,45 +1,46 @@
 package com.github.espiandev.showcaseview.sample;
 
 import android.os.Bundle;
-import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.github.espiandev.showcaseview.ShowcaseView;
 
-public class ActionItemsSampleActivity extends SherlockActivity {
+public class ActionItemsSampleActivity extends FragmentActivity {
 
-	ShowcaseView sv;
-	ShowcaseView.ConfigOptions mOptions = new ShowcaseView.ConfigOptions();
+    ShowcaseView sv;
+    ShowcaseView.ConfigOptions mOptions = new ShowcaseView.ConfigOptions();
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		mOptions.block = false;
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        mOptions.block = false;
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getSupportMenuInflater().inflate(R.menu.menu, menu);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
 
-		sv = ShowcaseView.insertShowcaseViewWithType(ShowcaseView.ITEM_ACTION_OVERFLOW, R.id.menu_item1, this,
-				"ShowcaseView & action items", "Try touching action items to showcase them", mOptions);
-		//sv.setTextColors(Color.GREEN, Color.GRAY);
+        sv = ShowcaseView.insertShowcaseViewWithType(ShowcaseView.ITEM_ACTION_OVERFLOW, R.id.menu_item1, this,
+                "ShowcaseView & action items", "Try touching action items to showcase them", mOptions);
+        //sv.setTextColors(Color.GREEN, Color.GRAY);
 
-		return super.onCreateOptionsMenu(menu);
-	}
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int itemId = item.getItemId();
-		if (itemId == android.R.id.home)
-			sv.setShowcaseItem(ShowcaseView.ITEM_ACTION_HOME, 0, this);
-		else if (itemId == R.id.menu_item1)
-			sv.setShowcaseItem(ShowcaseView.ITEM_ACTION_ITEM, R.id.menu_item1, this);
-		else if (itemId == R.id.menu_item2)
-			sv.setShowcaseItem(ShowcaseView.ITEM_TITLE_OR_SPINNER, 0, this);
-		return true;
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home)
+            sv.setShowcaseItem(ShowcaseView.ITEM_ACTION_HOME, 0, this);
+        else if (itemId == R.id.menu_item1)
+            sv.setShowcaseItem(ShowcaseView.ITEM_ACTION_ITEM, R.id.menu_item1, this);
+        else if (itemId == R.id.menu_item2)
+            sv.setShowcaseItem(ShowcaseView.ITEM_TITLE_OR_SPINNER, 0, this);
+        return true;
+    }
 
 }
