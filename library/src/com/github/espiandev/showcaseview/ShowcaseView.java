92bc4ef7d81bd67e9b53aed6eef1a4f0583fcb54
Matt Royal <matt@callpod.com>
2013-10-22 14:00:22 -0700

    Fix TPR.QE - Defect # 23139
    This should resolve JIRA bug AN-353 (and duplicates). May also resolve
    AN-350

--- library/src/com/github/espiandev/showcaseview/ShowcaseView.java
+++ library/src/com/github/espiandev/showcaseview/ShowcaseView.java
@@ -975,7 +975,7 @@ public class ShowcaseView extends RelativeLayout implements
 			// Bottom
 			textX = ((float) voidedOverlay.right - voidedOverlay.left) / 2;
 			if (showcase.overlayArrowRotation >= 202.5 && showcase.overlayArrowRotation <= 337.5) {
-				textY = (float) voidedOverlay.top - (voidedOverlay.height() / 3);
+				textY = (float) voidedOverlay.top - (voidedOverlay.height() / 2);
 				textY = (textY >= (OK_BUTTON_HEIGHT * metricScale)) ? textY : (OK_BUTTON_HEIGHT * metricScale);
 			} else {
 				textY = (float) (2 * voidedOverlay.top) - voidedOverlay.bottom;
