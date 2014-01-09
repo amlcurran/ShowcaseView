2ac9ea1c131de27216d062e6544c6bf86e9f97e1
bwp <haleokekahuna@gmail.com>
2014-01-09 11:35:05 -0700

    updated some samples

--- sample/src/com/espian/showcaseview/sample/animations/AnimationSampleActivity.java
+++ sample/src/com/espian/showcaseview/sample/animations/AnimationSampleActivity.java
@@ -28,7 +28,7 @@ public class AnimationSampleActivity extends Activity {
         final TextView textView2 = (TextView) findViewById(R.id.textView2);
         final TextView textView3 = (TextView) findViewById(R.id.textView3);
 
-        showcaseView = ShowcaseView.insertShowcaseView(new ViewTarget(findViewById(R.id.textView)), this, false);
+        showcaseView = ShowcaseView.insertShowcaseView(new ViewTarget(findViewById(R.id.textView)), this);
         showcaseView.overrideOKButtonClick(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
