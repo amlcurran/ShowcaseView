085e8cbc52800c67ae46cb7623d052fbb4eedcfd
tato469 <tato469@gmail.com>
2014-07-08 08:59:51 +0200

    Fixed error java.lang.OutOfMemoryError
    Fixed error java.lang.OutOfMemoryError when the function updateBitmap() was called.

--- library/src/main/java/com/github/amlcurran/showcaseview/ShowcaseView.java
+++ library/src/main/java/com/github/amlcurran/showcaseview/ShowcaseView.java
@@ -166,7 +166,10 @@ public class ShowcaseView extends RelativeLayout
 
     private void updateBitmap() {
         if (bitmapBuffer == null || haveBoundsChanged()) {
+            if(bitmapBuffer != null)
+        		bitmapBuffer.recycle();
             bitmapBuffer = Bitmap.createBitmap(getMeasuredWidth(), getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
         }
     }
 
