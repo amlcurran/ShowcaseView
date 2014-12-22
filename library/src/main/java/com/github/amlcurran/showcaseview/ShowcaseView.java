27d76b3bd19dd7e77de2d3fb9ff35482e6a78679
masterchop <choplabalagun@gmail.com>
2014-12-22 10:31:19 -0600

    Update ShowcaseView.java

--- library/src/main/java/com/github/amlcurran/showcaseview/ShowcaseView.java
+++ library/src/main/java/com/github/amlcurran/showcaseview/ShowcaseView.java
@@ -383,6 +383,12 @@ public class ShowcaseView extends RelativeLayout
      * It is recommended that you use this Builder class.
      */
     public static class Builder {
+            
+            //add on
+        public Builder setScaleMultiplier(float multiplier){
+            showcaseView.setScaleMultiplier(multiplier);
+            return this;
+        }
 
         final ShowcaseView showcaseView;
         private final Activity activity;
