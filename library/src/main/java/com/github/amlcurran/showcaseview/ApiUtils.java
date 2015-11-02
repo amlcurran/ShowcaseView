218599c932226ba94885839485e589ffd5c703e1
Alex Curran <aml.curran@gmail.com>
2015-11-02 20:23:34 +0000

    Remove unused method in ApiUtils

--- library/src/main/java/com/github/amlcurran/showcaseview/ApiUtils.java
+++ library/src/main/java/com/github/amlcurran/showcaseview/ApiUtils.java
@@ -16,9 +16,7 @@
 
 package com.github.amlcurran.showcaseview;
 
-import android.annotation.TargetApi;
 import android.os.Build;
-import android.view.View;
 
 public class ApiUtils {
 
@@ -30,10 +28,4 @@ public class ApiUtils {
         return isCompatWith(Build.VERSION_CODES.HONEYCOMB);
     }
 
-    @TargetApi(14)
-    public void setFitsSystemWindowsCompat(View view) {
-        if (isCompatWith(Build.VERSION_CODES.ICE_CREAM_SANDWICH)) {
-            view.setFitsSystemWindows(false);
-        }
-    }
 }
