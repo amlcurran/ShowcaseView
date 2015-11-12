5e0bfe7d6ec34c9a46726344e7d04e2b569fc493
Alex Curran <aml.curran@gmail.com>
2015-11-12 07:16:57 +0100

    Remove unnecessary modifiers

--- library/src/main/java/com/github/amlcurran/showcaseview/OnShowcaseEventListener.java
+++ library/src/main/java/com/github/amlcurran/showcaseview/OnShowcaseEventListener.java
@@ -25,23 +25,23 @@ public interface OnShowcaseEventListener {
      * Called when the ShowcaseView has been told to hide. Use {@link #onShowcaseViewDidHide(ShowcaseView)}
      * if you want to know when the ShowcaseView has been fully hidden.
      */
-    public void onShowcaseViewHide(ShowcaseView showcaseView);
+    void onShowcaseViewHide(ShowcaseView showcaseView);
 
     /**
      * Called when the animation hiding the ShowcaseView has finished, and it is no longer visible on the screen.
      */
-    public void onShowcaseViewDidHide(ShowcaseView showcaseView);
+    void onShowcaseViewDidHide(ShowcaseView showcaseView);
 
     /**
      * Called when the ShowcaseView is shown.
      */
-    public void onShowcaseViewShow(ShowcaseView showcaseView);
+    void onShowcaseViewShow(ShowcaseView showcaseView);
 
     /**
      * Empty implementation of OnShowcaseViewEventListener such that null
      * checks aren't needed
      */
-    public static final OnShowcaseEventListener NONE = new OnShowcaseEventListener() {
+    OnShowcaseEventListener NONE = new OnShowcaseEventListener() {
         @Override
         public void onShowcaseViewHide(ShowcaseView showcaseView) {
 
