45978f41223dd1e587044db6751ce5e35ef090f8
Alex Curran <aml.curran@gmail.com>
2015-11-12 07:21:24 +0100

    Remove bad doc message

--- library/src/main/java/com/github/amlcurran/showcaseview/NewShowcaseDrawer.java
+++ library/src/main/java/com/github/amlcurran/showcaseview/NewShowcaseDrawer.java
@@ -20,9 +20,6 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
-/**
- * Created by curraa01 on 13/10/2013.
- */
 class NewShowcaseDrawer extends StandardShowcaseDrawer {
 
     private static final int ALPHA_60_PERCENT = 153;
@@ -64,8 +61,4 @@ class NewShowcaseDrawer extends StandardShowcaseDrawer {
         return innerRadius;
     }
 
-    @Override
-    public void setBackgroundColour(int backgroundColor) {
-        this.backgroundColour = backgroundColor;
-    }
 }
