770f77944c1b28cb9f32f3b6eb6461461a9d7441
Alex Curran <aml.curran@gmail.com>
2016-04-06 20:31:51 +0100

    Adds some API doc

--- sample/src/main/java/com/github/amlcurran/showcaseview/sample/ViewTargets.java
+++ sample/src/main/java/com/github/amlcurran/showcaseview/sample/ViewTargets.java
@@ -7,8 +7,17 @@ import com.github.amlcurran.showcaseview.targets.ViewTarget;
 
 import java.lang.reflect.Field;
 
+/**
+ * A collection of not-officially supported ViewTargets. Use them at your own risk!
+ */
 public class ViewTargets {
 
+    /**
+     * Highlight the navigation button (the Up or Navigation drawer button) in a Toolbar
+     * @param toolbar The toolbar to search for the view in
+     * @return the {@link ViewTarget} to supply to a {@link com.github.amlcurran.showcaseview.ShowcaseView}
+     * @throws MissingViewException when the view couldn't be found. Raise an issue on Github if you get this!
+     */
     public static ViewTarget navigationButtonViewTarget(Toolbar toolbar) throws MissingViewException {
         try {
             Field field = Toolbar.class.getDeclaredField("mNavButtonView");
